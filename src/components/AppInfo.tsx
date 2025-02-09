b0cbd0cdb31f85ef87d2886cc45dfb17edd117d4
mcell <minorcell6789@gmail.com>
2025-02-09 15:19:55 +0800

    fix(font): add preload link for font and fix the bug of logo load.

--- src/components/AppInfo.tsx
+++ src/components/AppInfo.tsx
@@ -6,12 +6,14 @@ function AppInfo() {
       <div className="max-w-4xl mx-auto text-center">
         <div className="flex flex-col items-center justify-center mb-8 sm:mb-12">
           <div className="relative">
-            <img
-              src={logo}
-              alt="io-logo"
-              className="w-24 sm:w-32 md:w-40 animate-pulse"
-            />
-            <div className="absolute inset-0 bg-blue-500 filter blur-xl opacity-50 animate-pulse"></div>
+            <div className="w-24 h-24 sm:h-32 md:h-40 sm:w-32 md:w-40">
+              <img
+                src={logo}
+                alt="io-logo"
+                className="w-24 sm:w-32 md:w-40 animate-pulse"
+              />
+            </div>
+            <div className="absolute inset-0 bg-blue-500 filter blur-2xl opacity-50 animate-pulse"></div>
           </div>
           <h1 className="mt-6 text-5xl sm:text-6xl md:text-7xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 font-SixtyfourConvergence">
